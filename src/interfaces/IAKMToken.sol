3f510b8bd33c8e8f6ed4b287b870f7b5b900e2a3
Elijah Thomas <zozothezombie@gmail.com>
2026-07-21 23:23:58 -0600

    refactor(interface): decouple IAKMToken from authorization layer

--- src/interfaces/IAKMToken.sol
+++ src/interfaces/IAKMToken.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {IAKMAuthorization} from "./IAKMAuthorization.sol";
-
 /// @title IAKMToken
-/// @notice ERC20 interface for the Akmena Token.
-interface IAKMToken is IERC20, IAKMAuthorization {
+/// @notice Immutable monetary layer interface for the Akmena Protocol.
+interface IAKMToken is IERC20 {
+
     /// @notice Returns the immutable maximum token supply.
     function maxSupply() external view returns (uint256);
 }
